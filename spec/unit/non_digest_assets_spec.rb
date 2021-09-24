88e349150e1e6a0f1996a6663608c2dc9046ece1
Matijs van Zuijlen <matijs@matijs.net>
2021-09-24 12:36:23 +0200

    Update coding style rules
    - Update some rules to match standardrb
    - Remove superflous configuration for Style/AndOr
    - Disable AmbiguousOperatorPrecedence
    - Fix offenses

--- spec/unit/non_digest_assets_spec.rb
+++ spec/unit/non_digest_assets_spec.rb
@@ -6,44 +6,44 @@ require "non-digest-assets"
 RSpec.describe NonDigestAssets do
   describe ".asset_selectors" do
     it "is an alternative reader for .whitelist" do
-      described_class.whitelist = %w(baz qux)
-      expect(described_class.asset_selectors).to eq %w(baz qux)
+      described_class.whitelist = %w[baz qux]
+      expect(described_class.asset_selectors).to eq %w[baz qux]
     end
   end
 
   describe ".asset_selectors=" do
     it "is an alternative setter for .whitelist" do
-      described_class.asset_selectors = %w(qux quuz)
-      expect(described_class.whitelist).to eq %w(qux quuz)
+      described_class.asset_selectors = %w[qux quuz]
+      expect(described_class.whitelist).to eq %w[qux quuz]
     end
   end
 
   describe ".filter_assets" do
     it "returns its arguments if there are no selected assets" do
       described_class.asset_selectors = []
-      expect(described_class.filter_assets(%w(foo bar))).to eq %w(foo bar)
+      expect(described_class.filter_assets(%w[foo bar])).to eq %w[foo bar]
     end
 
     it "returns only selected parts of arguments if there are selected assets" do
-      described_class.asset_selectors = %w(bar baz)
-      expect(described_class.filter_assets(%w(foo bar))).to eq ["bar"]
+      described_class.asset_selectors = %w[bar baz]
+      expect(described_class.filter_assets(%w[foo bar])).to eq ["bar"]
     end
 
     it "allows filtering using a regex" do
       described_class.asset_selectors = [/ba/]
-      expect(described_class.filter_assets(%w(foo bar ababa))).to eq %w(bar ababa)
+      expect(described_class.filter_assets(%w[foo bar ababa])).to eq %w[bar ababa]
     end
   end
 
   describe ".assets (deprecated)" do
     it "returns its arguments if there are no selected assets" do
       described_class.asset_selectors = []
-      expect(described_class.assets(%w(foo bar))).to eq %w(foo bar)
+      expect(described_class.assets(%w[foo bar])).to eq %w[foo bar]
     end
 
     it "returns only selected parts of arguments if there are selected assets" do
-      described_class.asset_selectors = %w(bar baz)
-      expect(described_class.assets(%w(foo bar))).to eq ["bar"]
+      described_class.asset_selectors = %w[bar baz]
+      expect(described_class.assets(%w[foo bar])).to eq ["bar"]
     end
   end
 end
