49bd7f650ddd025f6a9ee7834da3d1d3cfd3bd77
Brian Kephart <briantkephart@gmail.com>
2021-11-10 14:16:03 -0600

    Support Rails 7.0

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -8,15 +8,20 @@ RAILS_5_1_SPROCKETS_3_DIGEST =
   "f0d704deea029cf000697e2c0181ec173a1b474645466ed843eb5ee7bb215794"
 RAILS_6_SPROCKETS_4_DIGEST =
   "b324c44f04a0d0da658824105489a2676d49df561c3d06723770321fd441977c"
+RAILS_7_SPROCKETS_4_DIGEST =
+  "e0cf9d8fcb18bf7f909d8d91a5e78499f82ac29523d475bf3a9ab265d5e2b451"
 
 RSpec.describe "compiling assets using non-digest-assets", type: :aruba do
   let(:digest_key) do
-    # This assumes Rails 6 is tested in combination with Sprockets 4, and Rails
+    # This assumes Rails 6+ is tested in combination with Sprockets 4, and Rails
     # 5 is tested in combination with Sprockets 3
-    if Rails::VERSION::MAJOR >= 6
+    case Rails::VERSION::MAJOR
+    when 5
+      RAILS_5_1_SPROCKETS_3_DIGEST
+    when 6
       RAILS_6_SPROCKETS_4_DIGEST
     else
-      RAILS_5_1_SPROCKETS_3_DIGEST
+      RAILS_7_SPROCKETS_4_DIGEST
     end
   end
 
@@ -26,7 +31,7 @@ RSpec.describe "compiling assets using non-digest-assets", type: :aruba do
       " --skip-javascript"
     if Rails::VERSION::MAJOR >= 6
       flags += " --skip-webpack-install"
-      flags += " --no-skip-javascript" if Rails::VERSION::MINOR >= 1
+      flags += " --no-skip-javascript" if Rails.version.to_f >= 6.1
     end
     flags
   end
